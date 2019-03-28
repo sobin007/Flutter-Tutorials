aa3cdf55c9d958909a01e1a8e00de8d8c4e6ab1f
sobin007 <sobinmathew988@gmail.com>
2019-03-28 15:13:00 +0530

    interaction

--- layout/lib/main.dart
+++ layout/lib/main.dart
@@ -38,12 +38,7 @@ class MyApp extends StatelessWidget {
               ],
             ),
           ),
-          /*3*/
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          Text('41'),
+        FavoriteWidget(),
         ],
       ),
     );
@@ -117,4 +112,51 @@ class MyApp extends StatelessWidget {
       ],
     );
   }
+}
+
+class FavoriteWidget extends StatefulWidget {
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Row(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(0),
+            child: IconButton(
+              icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+              color: Colors.red[500],
+              onPressed: _toggledFavorite,
+            ),
+          ),
+          SizedBox(
+            width: 18,
+            child: Container(
+              child: Text('$_favoriteCount'),
+            ),
+            ),
+        ],
+    );
+  }
+
+  void _toggledFavorite() {
+    setState(() {
+      if(_isFavorited) {
+        _favoriteCount -=1;
+        _isFavorited =false;
+      }else{
+        _favoriteCount +=1;
+        _isFavorited =true;
+      }
+    });
+  }
+
 }
